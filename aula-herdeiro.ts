5240e88bfc6fb47d82888ed5e278a86dacec3510
Kleiton Serra <148584816+eukleiton@users.noreply.github.com>
2023-10-26 21:41:47 -0300

    Update aula-herdeiro.ts

--- aula-herdeiro.ts
+++ aula-herdeiro.ts
@@ -1,30 +1,30 @@
 export class Transporte {
     private _capacidade: number;
-  
+
     public get capacidade(): number {
-      return this._capacidade;
+        return this._capacidade;
     }
-  
+
     set capacidade(capacidade: number) {
-      this._capacidade = capacidade;
+        this._capacidade = capacidade;
     }
-  }
-  
-  export class Terrestre extends Transporte {
+}
+
+export class Terrestre extends Transporte {
     private _numRodas: number;
-  
+
     public get numRodas(): number {
-      return this._numRodas;
+        return this._numRodas;
     }
-  
+
     public set numRodas(numRodas: number) {
-      this._numRodas = numRodas;
+        this._numRodas = numRodas;
     }
-  
+
     public exibirInformacoes() {
-      console.log(
-        `Capacidade: ${this.capacidade},
+        console.log(
+            `Capacidade: ${this.capacidade},
         Rodas: ${this._numRodas}`
-      );
+        );
     }
-  }
+}
