2dd1f214f0354c03c4bd4a8a5c75651bb65309e0
Kleiton Serra <148584816+eukleiton@users.noreply.github.com>
2023-10-31 20:22:40 -0300

    att

--- encapsulamento/index.ts
+++ encapsulamento/index.ts
@@ -3,9 +3,19 @@ import { Endereco } from './models/endereco';
 import './style.css';
 
 let endereco = new Endereco();
+endereco.setBairro('Rocha');
+endereco.setRua('Brooklyn');
+endereco.setCidade('Rio De Janeiro');
+endereco.setEstado('RJ');
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>Encapsu<span>lamento </span></h1>
-<code> ${JSON.stringify(Endereco)} </code>
+<h1>${JSON.stringify(endereco)} </h1>
+<h1>${endereco.getBairro()} </h1>
+<h1>${endereco.getRua()} </h1>
+<h1>${endereco.getCidade()} </h1>
+<h1>${endereco.getEstado()} </h1>
 `;
+
+console.log(endereco);
--- encapsulamento/models/endereco.ts
+++ encapsulamento/models/endereco.ts
@@ -4,18 +4,41 @@ export class Endereco {
     private _cidade: string;
     private _estado: string;
   
-   // constructor(rua: string, bairro: string, cidade: string, estado: string) {
+    // constructor(rua: string, bairro: string, cidade: string, estado: string) {
     //  this._rua = rua;
     //  this._bairro = bairro;
     //  this._cidade = cidade;
     //  this._estado = estado;
-  // }
-    public get rua(): string{
+    // }
+    public getRua(): string {
       return this._rua;
     }
-    public set rua(rua: string){
+    public setRua(rua: string) {
       this._rua = rua;
     }
   
+    public getBairro(): string {
+      return this._bairro;
+    }
+  
+    public setBairro(bairro: string) {
+      this._bairro = bairro;
+    }
+  
+    public getCidade(): string {
+      return this._cidade;
+    }
+  
+    public setCidade(cidade: string) {
+      this._cidade = cidade;
+    }
+  
+    public getEstado(): string {
+      return this._estado;
+    }
+  
+    public setEstado(estado: string) {
+      this._estado = estado;
+    }
   }
   
